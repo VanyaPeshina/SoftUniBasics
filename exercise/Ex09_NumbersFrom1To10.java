f5cb46222840c63b6fe96541353ac069096eec84
Vanya Peshina <peshina_vanya@abv.bg>
2021-02-01 19:48:39 +0200

    Add files via upload

---
+++ exercise/Ex09_NumbersFrom1To10.java
@@ -0,0 +1,12 @@
+package com.soft.uni.fundamentals.exercise;
+
+public class Ex09_NumbersFrom1To10 {
+    public static void main(String[] args) {
+
+        int i = 1;
+        while (i < 11) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
